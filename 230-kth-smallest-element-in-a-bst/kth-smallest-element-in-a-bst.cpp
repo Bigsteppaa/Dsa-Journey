4b8923d4f7048832ff9fefc5ae9dfd6971c6abd0
Bigsteppaa <aryanmeenaexe@gmail.com>
2026-07-22 23:00:56 +0530

    Time: 0 ms (100.00%) | Memory: 24.5 MB (40.96%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -1,27 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-
-    int ans = 0;
-
     int kthSmallest(TreeNode* root, int k) {
-        int cnt = 0;
-        small(root, k, cnt);
-        return ans;
-    }
 
-    void small(TreeNode* node, int k, int& cnt){
 
-        if(!node) return;
+        if(!root) return true;
+         int ans = 0;
+         int cnt = 0;
+       
+        TreeNode* curr = root;
+       while(curr){
+          if(!curr->left){
+              cnt++;
+              if(cnt == k) ans = curr->val; 
+               
+               curr = curr->right;
+          }
+          else{
+            TreeNode* prev = curr->left;
 
-        small(node->left, k, cnt);
+        while(prev->right && prev->right != curr){
+            prev = prev->right;
+        }
+        if(!prev->right){
+            prev->right = curr;
+              curr = curr->left;
+        }
+        else{
+                prev->right = NULL;
+                cnt++;
+              if(cnt == k) ans = curr->val; 
+          
+                curr = curr->right;
 
-        cnt++;
-
-        if(cnt == k){
-            ans = node->val;
-            return;
         }
 
-        small(node->right, k, cnt);
+
+          }
+
+
+       }
+        return ans;
+        
     }
 };
