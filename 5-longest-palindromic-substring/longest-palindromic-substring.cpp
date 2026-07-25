bafeb56454488d6b6bcc62d1bdac48cc0a0d3b99
Bigsteppaa <aryanmeenaexe@gmail.com>
2026-07-25 22:39:20 +0530

    Time: 7 ms (85.30%) | Memory: 9.2 MB (92.06%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int mx = 1;
-        int st = 0;
-        int end = 0;
-
+       int st = 0;
+       
+       int mx = 1;
         for(int i = 0;i<s.size();i++){
-                
-            for(int j = i;j<s.size();j++){
-            if(palin(s,i,j)){
-                if(j-i+1>mx){
-                    mx = j-i+1;
-                    st = i;
-                    end = j;
-                }
+            int l = i-1;
+            int r = i+1;
+              
+            while(l>=0 && r<s.size()){
+                   if(s[l] != s[r]) break;
+                   if(r-l+1>mx){
+                    mx = r-l+1;
+                    st = l;
+                   }
 
+                 l--;
+                 r++;
             }
+             l = i;
+             r = i+1;
+
+            while(l>=0 && r<s.size()){
+                   if(s[l] != s[r]) break;
+                   if(r-l+1>mx){
+                    mx = r-l+1;
+                    st = l;
+                   }
+
+                 l--;
+                 r++;
             }
-           
-         }
-       return s.substr(st,end-st+1); }
 
 
-    bool palin(string &s,int l , int r){
-      
-       while(l<r){
-        if(s[l] != s[r]) return false;
-         l++;r--;
-       }
-        return true;
+
+        }
+      return s.substr(st,mx);  
     }
 };
