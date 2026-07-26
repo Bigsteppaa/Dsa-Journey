40defa7b39a2ca8d5d50e81aabbf8e44770ef5d6
Bigsteppaa <aryanmeenaexe@gmail.com>
2026-07-26 23:24:34 +0530

    Time: 11 ms (6.77%) | Memory: 10 MB (99.95%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+
+
+        vector<int> freq(128,0);
+
+        for(auto it : s){
+            freq[it]++;
+        }
+
+        auto cmp = [&](char a,char b){
+
+           if(freq[a] == freq[b]) return a<b;
+
+           return freq[a]>freq[b];
+
+        };
+        sort(s.begin(),s.end(),cmp);
+
+        return s;
+        
+    }
+};
