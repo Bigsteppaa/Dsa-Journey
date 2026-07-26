3ff0703b87d1a31f4fe03c76e808e4901bf42375
Bigsteppaa <aryanmeenaexe@gmail.com>
2026-07-27 00:36:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (86.23%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+
+        int cnt = 0;
+        int mx = 0;
+        for(auto it : s){
+            if(it == '(') cnt++;
+            else if (it == ')') cnt--;
+            mx = max(cnt,mx);
+        }
+      return mx;  
+    }
+};
