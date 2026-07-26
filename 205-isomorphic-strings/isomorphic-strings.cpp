c70686964ffc18c6cec1a1b69b5a62c1168284da
Bigsteppaa <aryanmeenaexe@gmail.com>
2026-07-26 18:41:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (58.01%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+
+          map<char,char> mp;
+
+          for(int i = 0;i<s.size();i++){
+
+           if(!mp[s[i]]){
+
+            for(auto it : mp){
+                if(it.second == t[i]) return false;
+            }
+
+
+            mp[s[i]] = t[i];
+           }
+           else{
+            if(mp[s[i]] != t[i]) return false;
+           }
+
+          } 
+
+
+
+      return true;  
+    }
+};
