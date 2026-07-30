7eaefc9128806b756fd4f054321894cb4fa35753
Bigsteppaa <aryanmeenaexe@gmail.com>
2026-07-30 17:32:37 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (31.28%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -8,6 +8,7 @@ public:
        queue<pair<int,int>> q;
       q.push({sr,sc});
       int org = image[sr][sc];
+      if(org == color) return image;
       image[sr][sc] = color;
                
 
@@ -26,7 +27,7 @@ public:
              for(int i = 0;i<4;i++){
                  int nr = r + drow[i];
                  int nc = c + dcol[i];
-                 if(nr>= 0 && nr<n && nc>= 0 && nc<m && image[nr][nc] == org && image[nr][nc] != color ){
+                 if(nr>= 0 && nr<n && nc>= 0 && nc<m && image[nr][nc] == org){
                     q.push({nr,nc});
                     image[nr][nc] = color;
                     
