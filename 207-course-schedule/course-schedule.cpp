b62ac85f40e4cbd0392b26aa4dee2ce195c0b29f
Bigsteppaa <aryanmeenaexe@gmail.com>
2026-08-06 23:42:48 +0530

    Time: 3 ms (75.20%) | Memory: 19.5 MB (48.05%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,37 @@
+class Solution {
+        private: bool dfs(int node,vector<vector<int>>& adj,vector<int> &vis,vector<int>&path){
+            vis[node] = 1;
+            path[node] = 1;
+
+            for(auto it : adj[node]){
+                if(!vis[it]) { if(dfs(it,adj,vis,path)) return true;
+                }
+                else if(path[it]) return true;
+            }
+
+           path[node] = 0;
+           return false;
+
+        }
+
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+     vector<vector<int>> adj(numCourses);
+     vector<int> vis(numCourses,0);
+     vector<int> path(numCourses,0);
+
+       for(auto &e : prerequisites) adj[e[0]].push_back(e[1]);
+
+       for(int i = 0;i<numCourses;i++){
+               if(!vis[i]){
+                if(dfs(i,adj,vis,path)) return false;
+                
+               }
+
+       }
+
+        
+
+      return true;  
+    }
+};
