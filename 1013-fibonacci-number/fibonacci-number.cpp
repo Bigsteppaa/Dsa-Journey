965714d9a210f8ee78c6abb43936578d7bab6987
Bigsteppaa <aryanmeenaexe@gmail.com>
2026-08-22 01:15:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (20.20%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,18 +1,21 @@
 class Solution {
   
-  int fibo(int n, vector<int> &dp){
-    if(n<=1) return n;
-     if(dp[n] != -1) return dp[n];
-
-     return dp[n] = fibo(n-1,dp) + fibo(n-2,dp);
-
-  }
+  
 
 
 public:
     int fib(int n) {
+        if(n == 0) return 0;
 
-        vector<int> dp(n+1,-1);
-        return fibo(n,dp);
+        vector<int> dp(n+1);
+
+     dp[0] = 0;
+    dp[1] = 1;
+    for(int i = 2;i<=n;i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+
+
+        return dp[n];
     }
 };
