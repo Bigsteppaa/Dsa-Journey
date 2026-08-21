31e18760129854f8bb7577017acf9df2d298d1f3
Bigsteppaa <aryanmeenaexe@gmail.com>
2026-08-21 22:28:22 +0530

    Time: 3 ms (68.51%) | Memory: 12.8 MB (97.31%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -2,20 +2,21 @@ class Solution {
   
   public: void solve(int n,int open,int close,string &s,vector<string> &ans){
 
- if(open > n || close > n) return;
+ 
 
     if(open == n && close == n){
     ans.push_back(s);
     return;} 
 
-   if(s.size() != 0 && open>close){
+   if(open>close){
            s.push_back(')');       
            solve(n,open,close+1,s,ans);
            s.pop_back();       
    }
-          s.push_back('(');
+         if(open<n){ s.push_back('(');
           solve(n,open+1,close,s,ans);
            s.pop_back();
+         }
   } 
 
 
