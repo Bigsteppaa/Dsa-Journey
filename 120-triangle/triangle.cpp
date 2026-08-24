c7603942607dbab3e912ba5e4d465b2956feae2a
Bigsteppaa <aryanmeenaexe@gmail.com>
2026-08-25 02:41:52 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (67.03%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -4,27 +4,29 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n,0));
+        vector<int> dp(n,0);
 
         for(int i = 0;i<triangle[n-1].size();i++){
-           dp[n-1][i] = triangle[n-1][i];
+           dp[i] = triangle[n-1][i];
 
         }
 
 
     
         for(int i = n-2;i>=0;i--){
+            vector<int> temp(n,0);
             for(int j = i;j>=0;j--){
 
-         int left = triangle[i][j] + dp[i+1][j];
+         int left = triangle[i][j] + dp[j];
        
-         int right = triangle[i][j] + dp[i+1][j+1];
+         int right = triangle[i][j] + dp[j+1];
       
-        dp[i][j]  = min(left,right);    
+        temp[j]  = min(left,right);    
 
             }
+            dp = temp;
         }
 
-     return dp[0][0];   
+     return dp[0];   
     }
 };
