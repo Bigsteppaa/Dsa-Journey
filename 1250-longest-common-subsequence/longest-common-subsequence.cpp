d81a0e85cdd4036358cbacf9cd8abf7c99966b93
Bigsteppaa <aryanmeenaexe@gmail.com>
2026-08-29 03:53:43 +0530

    Time: 43 ms (26.21%) | Memory: 27.6 MB (32.37%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -5,7 +5,7 @@ class Solution {
   if(ind1<0 || ind2<0) return 0;
   if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
 
-  if(s1[ind1] == s2[ind2]) return 1 + f(ind1-1,ind2-1,s1,s2,dp);
+  if(s1[ind1] == s2[ind2]) return dp[ind1][ind2] = 1 + f(ind1-1,ind2-1,s1,s2,dp);
 
   return dp[ind1][ind2] = max(f(ind1-1,ind2,s1,s2,dp),f(ind1,ind2-1,s1,s2,dp));
 
