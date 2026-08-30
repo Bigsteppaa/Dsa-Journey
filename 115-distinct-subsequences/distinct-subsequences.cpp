b33093e47f26cdee0af6c2373ea550ee6cb221d5
Bigsteppaa <aryanmeenaexe@gmail.com>
2026-08-30 19:00:53 +0530

    Time: 27 ms (70.95%) | Memory: 44.2 MB (31.30%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,26 +1,34 @@
 class Solution {
-
-int fu(int ind1,int ind2,string &s, string &f,vector<vector<int>> &dp){
-
- if(ind2 == 0) return 1;
- if(ind1 == 0) return 0;
- if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
- 
- if(s[ind1-1] == f[ind2-1]) return dp[ind1][ind2] = fu(ind1-1,ind2-1,s,f,dp) + fu(ind1-1,ind2,s,f,dp);
-
-return dp[ind1][ind2] = fu(ind1-1,ind2,s,f,dp);
-
-}
-
-
 public:
     int numDistinct(string s, string t) {
 
         int n = s.size();
         int m = t.size();
 
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return fu(n,m,s,t,dp);
-        
+        const long long LIM = INT_MAX;
+
+        vector<vector<long long>> dp(
+            n + 1, vector<long long>(m + 1, 0)
+        );
+
+        for(int i = 0; i <= n; i++)
+            dp[i][0] = 1;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+
+                if(s[i-1] == t[j-1]) {
+                    dp[i][j] = min(
+                        LIM,
+                        dp[i-1][j-1] + dp[i-1][j]
+                    );
+                }
+                else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
