a9a64b54d6a8107155939c38a6e9cdf2880db40b
Bigsteppaa <aryanmeenaexe@gmail.com>
2026-08-30 19:22:53 +0530

    Time: 11 ms (87.72%) | Memory: 9 MB (91.46%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -7,28 +7,27 @@ public:
 
         const long long LIM = INT_MAX;
 
-        vector<vector<long long>> dp(
-            n + 1, vector<long long>(m + 1, 0)
-        );
+        vector<long long> dp(m + 1, 0),curr(m + 1, 0);
 
-        for(int i = 0; i <= n; i++)
-            dp[i][0] = 1;
+           dp[0] = 1;
+           curr[0] = 1;
 
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= m; j++) {
 
                 if(s[i-1] == t[j-1]) {
-                    dp[i][j] = min(
+                    curr[j] = min(
                         LIM,
-                        dp[i-1][j-1] + dp[i-1][j]
+                        dp[j-1] + dp[j]
                     );
                 }
                 else {
-                    dp[i][j] = dp[i-1][j];
+                    curr[j] = dp[j];
                 }
             }
+            dp = curr;
         }
 
-        return dp[n][m];
+        return dp[m];
     }
 };
